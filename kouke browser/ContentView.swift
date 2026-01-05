e25ef6b712309d9178b643b659a7db1819f321a1
KoukeNeko <111033412+KoukeNeko@users.noreply.github.com>
2026-01-05 20:34:29 +0800

    Initial Commit

---
+++ kouke browser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kouke browser
+//
+//  Created by doeshing on 2026/1/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kouke browser/kouke_browserApp.swift
@@ -0,0 +1,17 @@
+//
+//  kouke_browserApp.swift
+//  kouke browser
+//
+//  Created by doeshing on 2026/1/5.
+//
+
+import SwiftUI
+
+@main
+struct kouke_browserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
